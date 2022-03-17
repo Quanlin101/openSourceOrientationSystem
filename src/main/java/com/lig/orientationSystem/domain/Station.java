5d7bdb4a4a6e96f36f035aeb01c609af43de1188
lig <3195719673@qq.com>
2022-03-17 23:17:55 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/lig/orientationSystem/domain/Station.java
+++ src/main/java/com/lig/orientationSystem/domain/Station.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Station {
-//    还是建一个list存？
+    //    还是建一个list存？
     //岗位的序号，和前端对应，从一开始
 //    private Integer number;
     //岗位的描述
@@ -21,14 +21,30 @@ public class Station {
 
     //初始化岗位列表
     //前端、产品、后端、运营、UI
-    public static ArrayList<Station> initStationList(){
+    public static ArrayList<Station> initStationList() {
 
         ArrayList<Station> arrayList = new ArrayList<>();
-        arrayList.add(new Station("前端","前端"));
-        arrayList.add(new Station("产品","产品"));
-        arrayList.add(new Station("后端","后端"));
-        arrayList.add(new Station("运营","运营"));
-        arrayList.add(new Station("UI","UI"));
+        arrayList.add(new Station("前端", "（以下为岗位介绍，具体要求可咨询面试官）\n" +
+                "-前端核心模块的设计、开发，对代码质量及进度负责\n" +
+                "-上承产品文档，下输测试原型，紧密配合后端对接，参与产品开发实战\n" +
+                "-关注前沿技术，及时转化为生产\n" +
+                "-技能：html&css&javascript、前端框架、算法等"));
+        arrayList.add(new Station("产品", "（以下为岗位介绍，具体要求可咨询面试官）\n" +
+                "-负责互联网产品的设计和推广，为产品的整个生命周期负责\n" +
+                "-发现需求，解决需求，并用落地产品实现\n" +
+                "-运用适当的工具完成各类产品文档，同时进行项目管理\n" +
+                "技能：沟通、协作、洞察力、工具使用等"));
+        arrayList.add(new Station("后端", "后端"));
+        arrayList.add(new Station("运营", "（以下为岗位介绍，具体要求可咨询面试官）\n" +
+                "-配合产品经理、前端，对程序&网页的后台设计负责\n" +
+                "-维护数据库及系统的日常稳定\n" +
+                "-技能：数据库、编程语言、算法等"));
+        arrayList.add(new Station("UI", "（以下为岗位介绍，具体要求可咨询面试官）\n" +
+                "- 负责产品视觉、交互设计，对产品的界面、操作逻辑负责\n" +
+                "- 辅助产品分析需求，定位产品视觉元素，作为需求与具体实现的桥梁\n" +
+                "- 熟练使用所需工具以表现设计，熟悉各平台设计规则，为用户着想的思维\n" +
+                "- 熟练掌握与产品和前端对线能力\n" +
+                "- 技能：Ps、Ai、Ae、Axure、审美、设计规范 "));
         return arrayList;
     }
 }
--- src/main/java/com/lig/orientationSystem/service/impl/InterviewerServiceImpl.java
+++ src/main/java/com/lig/orientationSystem/service/impl/InterviewerServiceImpl.java
@@ -28,46 +28,45 @@ public class InterviewerServiceImpl extends ServiceImpl<InterviewerMapper, Inter
 
 
     //面试官是否存在
-    public Integer isExist(String UserId){
+    public Integer isExist(String UserId) {
         return interviewerMapper.isExist(UserId);
     }
 
     //分页查看简历
-    public IPage<Resume> readResume(String userId, int current, int size, int status){
+    public IPage<Resume> readResume(String userId, int current, int size, int status) {
 //        interviewerMapper.readResume(interviewerId);
         boolean checked;
         boolean interview;
 
-        Page page = new Page(current,size);
-        if (status == 1){
+        Page page = new Page(current, size);
+        if (status == 1) {
             checked = false;
             interview = false;
-        }else if (status == 2){
+        } else if (status == 2) {
             checked = true;
             interview = false;
-        }else{
+        } else {
             checked = true;
             interview = true;
         }
-        IPage resumePage = interviewerMapper.readResume(page, userId, checked, interview,Resume.thisTimeProject);
+        IPage resumePage = interviewerMapper.readResume(page, userId, checked, interview, Resume.thisTimeProject);
         return resumePage;
     }
 
     //第一次查看简历，状态设为已读
-    public void checkResume(){
+    public void checkResume() {
 
     }
 
     //写面评 + 是否通过
-    public MethodPassWrapper saveEvaluation(int resumeId, String evaluation, boolean pass){
-        if (evaluation==null){
+    public MethodPassWrapper saveEvaluation(int resumeId, String evaluation, boolean pass) {
+        if (evaluation == null) {
             methodPassWrapper.setSuccess(false);
             methodPassWrapper.setDesc("面评不能为空");
-        }
-        else {
+        } else {
             boolean saveEvaluation = interviewerMapper.saveEvaluation(resumeId, evaluation, pass);
 
-            if (!saveEvaluation){
+            if (!saveEvaluation) {
                 methodPassWrapper.setSuccess(false);
                 methodPassWrapper.setDesc("持久化面评失败");
                 return methodPassWrapper;
@@ -82,21 +81,21 @@ public class InterviewerServiceImpl extends ServiceImpl<InterviewerMapper, Inter
         Interviewer interviewer = interviewerMapper.selectById(userId);
         return interviewer;
     }
+
     @Transactional
     //移交简历
-    public MethodPassWrapper changeInterviewer(String postPhoneNumber, String phoneNumber, int resumeId){
+    public MethodPassWrapper changeInterviewer(String postPhoneNumber, String phoneNumber, int resumeId) {
         Interviewer interviewer = interviewerMapper.selectByPhoneNumber(postPhoneNumber);
         Interviewer newInterviewer = interviewerMapper.selectByPhoneNumber(phoneNumber);
         int change = interviewer.getChange();
-        if (change>0){
+        if (change > 0) {
             System.out.println(newInterviewer.getInterviewerId());
-           interviewerMapper.changeInterviewer(newInterviewer.getInterviewerId(), resumeId);
-           change = change - 1;
-           interviewerMapper.updateChange(interviewer.getInterviewerId(),change);
-           interviewerMapper.upUndone(phoneNumber);
-           interviewerMapper.downUndone(postPhoneNumber);
-        }
-        else {
+            interviewerMapper.changeInterviewer(newInterviewer.getInterviewerId(), resumeId);
+            change = change - 1;
+            interviewerMapper.updateChange(interviewer.getInterviewerId(), change);
+            interviewerMapper.upUndone(phoneNumber);
+            interviewerMapper.downUndone(postPhoneNumber);
+        } else {
             methodPassWrapper.setSuccess(false);
             methodPassWrapper.setDesc("您已经没有转送机会:)");
             return methodPassWrapper;
@@ -109,40 +108,46 @@ public class InterviewerServiceImpl extends ServiceImpl<InterviewerMapper, Inter
     String agentid;
     @Autowired
     RestTemplate restTemplate;
+
     //WX推送消息收到简历的消息
     public boolean sendMessage(String userId, Resume resume) {
         JSONObject contents = new JSONObject();
         String gender = resume.getGender();
-        if ("1".equals(gender.toString())){
+        if ("1".equals(gender.toString())) {
             gender = "男";
-        }else {
+        } else {
             gender = "女";
         }
-        contents.put("content","您收到一份简历，请及时处理。"+
-                    "姓名:" + resume.getName()+
-                     "性别:"+ gender +"\n"
-                    +"<a href=\"http://weather-report.xdwizz.top/#/interviewer/read?resumeId="+resume.getResumeId()+"\">点击已查看</a>\n"
-                    +"<a href=\"http://weather-report.xdwizz.top/#/interviewer/fill?name="+resume.getName()+"&gender=" + resume.getGender()
-                        +"&major="+resume.getMajor()+"&resumeId="+resume.getResumeId()+"&grade="+resume.getGrade()+"\">点击面评</a>"
+        contents.put("content", "您收到一份简历，请及时处理。" +
+                "姓名:" + resume.getName() +
+                "\n性别:" + gender +
+                "\n年级:" + resume.getGrade() +
+                "\n手机号:" + resume.getPhoneNumber() +
+                "\nQQ:" + resume.getQq() +
+                "\n自述" + resume.getPractice() +
+                "\n<a href=\""+ resume.getFileURL() + "\">点击查看项目经历</a>" +
+                "\n<a href=\"http://weather-report.xdwizz.top/#/interviewer/read?resumeId=" + resume.getResumeId() + "\">点击已查看</a>"
+                + "\n<a href=\"http://weather-report.xdwizz.top/#/interviewer/fill?name=" + resume.getName() + "&gender=" + resume.getGender()
+                + "&major=" + resume.getMajor() + "&resumeId=" + resume.getResumeId() + "&grade=" + resume.getGrade() + "\">点击面评</a>"
 
-                );
+        );
         String access_token = AccessTokenUtils.access_token;
 
 
-        String url="https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="
-                +access_token;
+        String url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="
+                + access_token;
 
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("touser",userId);
-        jsonObject.put("msgtype","text");
-        jsonObject.put("agentid",agentid);
-        jsonObject.put("text",contents);
+        jsonObject.put("touser", userId);
+        jsonObject.put("msgtype", "text");
+        jsonObject.put("agentid", agentid);
+        jsonObject.put("text", contents);
 
         ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, jsonObject.toString(), String.class);
         String body = responseEntity.getBody();
         JSONObject jsonBody = JSONObject.parseObject(body);
         String errcode = jsonBody.getString("errcode");
-        if (!"0".equals(errcode)){
+        if (!"0".equals(errcode)) {
             return false;
         }
         return true;
@@ -150,28 +155,28 @@ public class InterviewerServiceImpl extends ServiceImpl<InterviewerMapper, Inter
 
     //定时推送仍有未处理简历的面试官 没查看没面评的都推送！
     @Scheduled(cron = "0 0 8 * * ?")
-    public void delayEval(){
+    public void delayEval() {
         String access_token = AccessTokenUtils.access_token;
-        String url="https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="
-                +access_token;
-        JSONObject content  = new JSONObject();
+        String url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="
+                + access_token;
+        JSONObject content = new JSONObject();
         content.put("content", "您有尚未处理的简历，请及时处理\n:)");
         JSONObject jsonObject = new JSONObject();
-        String [] users = interviewerMapper.selectUndonePerson();
+        String[] users = interviewerMapper.selectUndonePerson();
         String userString = "";
-        for (String user:users) {
-            userString =  userString.concat(user+"|");
+        for (String user : users) {
+            userString = userString.concat(user + "|");
         }
 //        System.out.println(userString);
-        if ("".equals(userString)){
+        if ("".equals(userString)) {
 //            System.out.println("全部完成辣");
             return;
         }
-        userString= userString.substring(0,userString.length()-1);
-        jsonObject.put("touser",userString);
-        jsonObject.put("msgtype","text");
-        jsonObject.put("agentid",agentid);
-        jsonObject.put("text",content);
+        userString = userString.substring(0, userString.length() - 1);
+        jsonObject.put("touser", userString);
+        jsonObject.put("msgtype", "text");
+        jsonObject.put("agentid", agentid);
+        jsonObject.put("text", content);
 //        System.out.println(jsonObject.toString());
         ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, jsonObject.toString(), String.class);
 //        System.out.println(responseEntity);
@@ -186,14 +191,14 @@ public class InterviewerServiceImpl extends ServiceImpl<InterviewerMapper, Inter
     //设置已面试
     public MethodPassWrapper setCheck(String resumeId) {
         Resume resume = interviewerMapper.selectResumeById(resumeId);
-        if (resume == null){
+        if (resume == null) {
             methodPassWrapper.setSuccess(false);
             methodPassWrapper.setDesc("简历id异常，已读更新失败");
             return methodPassWrapper;
         }
         interviewerMapper.setChecked(resumeId);
         resume = interviewerMapper.selectResumeById(resumeId);
-        if (!resume.isChecked()){
+        if (!resume.isChecked()) {
             methodPassWrapper.setSuccess(false);
             methodPassWrapper.setDesc("持久化数据异常，已读更新失败");
             return methodPassWrapper;
