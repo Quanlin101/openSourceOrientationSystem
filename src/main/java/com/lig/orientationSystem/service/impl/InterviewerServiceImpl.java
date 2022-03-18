ac45d1c9e1bdd88a7f99eddec70932186df4e8fd
lig <3195719673@qq.com>
2022-03-19 00:35:09 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/lig/orientationSystem/service/impl/InterviewerServiceImpl.java
+++ src/main/java/com/lig/orientationSystem/service/impl/InterviewerServiceImpl.java
@@ -137,9 +137,9 @@ public class InterviewerServiceImpl extends ServiceImpl<InterviewerMapper, Inter
                 "\nQQ:" + resume.getQq() +
                 "\n自述:" + resume.getPractice() +
                 "\n<a href=\""+ resume.getFileURL() + "\">点击查看项目经历</a>" +
-                "\n<a href=\"https://orientation-system.wizzstudio.com?resumeId=" + resume.getResumeId() + "\">点击已查看</a>"
-                + "\n<a href=\"https://orientation-system.wizzstudio.com?name=" + resume.getName() + "&gender=" + resume.getGender()
-                + "&major=" + resume.getMajor() + "&resumeId=" + resume.getResumeId() + "&grade=" + resume.getGrade() + "\">点击面评</a>"
+                "\n<a href=\"https://orientation-system.wizzstudio.com/#/interviewer/read?resumeId=" + resume.getResumeId() + "\">点击已查看</a>" +
+                "\n<a href=\"https://orientation-system.wizzstudio.com/#/interviewer/fill?name=" + resume.getName() + "&gender=" + resume.getGender() +
+                "&major=" + resume.getMajor() + "&resumeId=" + resume.getResumeId() + "&grade=" + resume.getGrade() + "\">点击面评</a>"
 
         );
         String access_token = AccessTokenUtils.access_token;
