e8f5699708cb00b1509e22eee6be03a0586768c4
lig <3195719673@qq.com>
2022-03-18 23:25:24 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/lig/orientationSystem/dao/IntervieweeMapper.java
+++ src/main/java/com/lig/orientationSystem/dao/IntervieweeMapper.java
@@ -21,7 +21,7 @@ public interface IntervieweeMapper extends BaseMapper<Resume> {
 //    Interviewer getProperInterviewer();
 
     //保存并自动分发
-    @Insert("INSERT INTO interviewer_resume   VALUES(((SELECT MIN(interviewer_id) FROM ( SELECT interviewer_id FROM interviewer WHERE resume_number = (SELECT MIN(resume_number) FROM (SELECT * FROM interviewer) t1 where receive = 1 and station = #{station})) t2 )),  #{resumeId})")
+    @Insert("INSERT INTO interviewer_resume   VALUES(((SELECT MIN(interviewer_id) FROM ( SELECT interviewer_id FROM interviewer WHERE resume_number = (SELECT MIN(resume_number) FROM interviewer where receive = 1 and station = #{station}) and receive = 1 and station = #{station}) t2 )),  #{resumeId})")
     boolean distributeResume(int resumeId, String station);
     //分发更新面试官的简历数量
     @Update("update interviewer set resume_number = resume_number + 1, undone_number = undone_number + 1 where interviewer_id = (select interviewer_id from interviewer_resume where resume_id = #{resumeId})")
