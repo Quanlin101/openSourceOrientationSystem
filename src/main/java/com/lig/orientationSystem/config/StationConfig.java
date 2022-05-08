e548ddac16608d40963fbebf7ab239c4cf174918
Quanlin101 <80079375+Quanlin101@users.noreply.github.com>
2022-05-08 11:55:17 +0800

    Update StationConfig.java

--- src/main/java/com/lig/orientationSystem/config/StationConfig.java
+++ src/main/java/com/lig/orientationSystem/config/StationConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
 
+
+//岗位信息的改动只在内存生效，数据库中没表 服务器重启回返回到初始状态
+//更改岗位并确定长期不变的话建议在station的list初始化那里进行修改
 @Configuration
 public class StationConfig {
     StationList stationArrayList = new StationList();
