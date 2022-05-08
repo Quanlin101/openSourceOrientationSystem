2b095eb0a61bf90dc14e81c281ecd7aae81d5e03
Quanlin101 <80079375+Quanlin101@users.noreply.github.com>
2022-05-08 12:04:18 +0800

    Update MethodPassWrapper.java

--- src/main/java/com/lig/orientationSystem/domain/MethodPassWrapper.java
+++ src/main/java/com/lig/orientationSystem/domain/MethodPassWrapper.java
@@ -3,6 +3,8 @@ package com.lig.orientationSystem.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+//用于controller层和service层的数据传递
 @Data
 @NoArgsConstructor
 public class MethodPassWrapper<T> {
